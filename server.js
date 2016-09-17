1286f1cc9bc2385d3c6ec6db4e60b050d7e98721
Sandip20 <sandipp017@gmail.com>
2016-09-18 01:09:48 +0530

    updated

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ app.set('port', (process.env.PORT || 5000));
 
 // views is directory for all template files
 
-app.get('/cool', function(request, response) {
+apiRoutes.get('/cool', function(request, response) {
   response.send(cool());
 });
 
+app.use('/api', apiRoutes);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
