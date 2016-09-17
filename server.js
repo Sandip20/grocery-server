84d294a7413df2c174999f0122ed6cea88399820
Sandip20 <sandipp017@gmail.com>
2016-09-18 01:12:51 +0530

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
-
+var apiRoutes = express.Router();
 
 app.set('port', (process.env.PORT || 5000));
 
