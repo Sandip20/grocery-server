5c61ebf9279e2456e48ddc77b983f2fc72632139
Sandip20 <sandipp017@gmail.com>
2016-09-18 00:55:55 +0530

    updated

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // set up ========================
         var express  = require('express');
-        var app      = express();    
-        app.get('/open',function (req,res) {
+        var app      = express();  
+        var apiRoutes = express.Router();
+            
+        apiRoutes.get('/open',function (req,res) {
 
-        res.json({Success:'true'});
-        })
-        
+        res.send('hellop');
+        });
+        app.use('/api', apiRoutes);
 
-     app.use('/api', apiRoutes);
+    
     app.listen(8081);
         console.log("App listening on port 8081");    
 
