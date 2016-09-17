06d55d4c6cf9d0e4dece2dc59d7ad79f861c324c
Sandip20 <sandipp017@gmail.com>
2016-09-18 01:06:36 +0530

    updated

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-// set up ========================
-        var express  = require('express');
-        var app      = express();  
-        var apiRoutes = express.Router();
-            
-        apiRoutes.get('/open',function (req,res) {
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-        res.send('hellop');
-        });
-        app.use('/api', apiRoutes);
 
-    
-    app.listen(8081);
-        console.log("App listening on port 8081");    
 
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
