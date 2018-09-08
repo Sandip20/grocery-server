a3b1219e12a3460cbd9b2d6fbca87539ac883ecd
Sandip20 <sandipp017@gmail.com>
2018-09-08 14:40:27 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secret': 'devdacticIsAwesome',
   //'database': 'mongodb://127.0.0.1/node-grocery-mongo'
- 'database':'mongodb://sandip:sandip123@ds033046.mlab.com:33046/node-grocery-mongo'
+ 'database':'mongodb://steem:steem123@ds249942.mlab.com:49942/steemitin'
   };
